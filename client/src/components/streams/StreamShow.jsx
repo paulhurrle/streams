c623e29cdd21cf711eb40702ccc14747a310fe7f
paulhurrle <paul_hurrle@yahoo.com>
2019-02-17 14:03:26 -0700

    add streaming logic using flv.js

--- client/src/components/streams/StreamShow.jsx
+++ client/src/components/streams/StreamShow.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import flv from 'flv.js';
 import { getStream } from '../../actions';
 
 class StreamShow extends Component {
-  componentDidMount() {
-    this.props.getStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    const { id } = this.props.match.params;
+
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
   render() {
     const { stream } = this.props;
     if (!stream) {
@@ -14,6 +44,7 @@ class StreamShow extends Component {
     }
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{stream.title}</h1>
         <h5>{stream.description}</h5>
       </div>
